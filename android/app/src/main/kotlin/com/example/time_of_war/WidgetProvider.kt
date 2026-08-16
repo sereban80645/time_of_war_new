bd3d1aba4375a94ac4bdaea0475ba13b6b7d090a
sereban80645 <sereban190@gmail.com>
2026-08-16 10:45:55 +0300

    Add PendingIntent for widget click action

--- android/app/src/main/kotlin/com/example/time_of_war/WidgetProvider.kt
+++ android/app/src/main/kotlin/com/example/time_of_war/WidgetProvider.kt
@@ -1,7 +1,9 @@
 package com.example.time_of_war
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
 import android.widget.RemoteViews
@@ -33,6 +35,28 @@ class WidgetProvider : HomeWidgetProvider() {
                     Uri.parse(imagePath)
                 )
             }
+
+            val intent = Intent(
+                context,
+                MainActivity::class.java
+            ).apply {
+                flags =
+                    Intent.FLAG_ACTIVITY_NEW_TASK or
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }
+
+            val pendingIntent = PendingIntent.getActivity(
+                context,
+                appWidgetId,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or
+                    PendingIntent.FLAG_IMMUTABLE
+            )
+
+            views.setOnClickPendingIntent(
+                R.id.widget_image,
+                pendingIntent
+            )
 
             appWidgetManager.updateAppWidget(
                 appWidgetId,
