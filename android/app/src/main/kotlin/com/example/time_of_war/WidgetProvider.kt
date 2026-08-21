bb2b7d3fb375e8b0b1d8e6d9046a6b2a9441bbca
sereban80645 <sereban190@gmail.com>
2026-08-21 13:41:23 +0300

    Update WidgetProvider.kt

--- android/app/src/main/kotlin/com/example/time_of_war/WidgetProvider.kt
+++ android/app/src/main/kotlin/com/example/time_of_war/WidgetProvider.kt
@@ -1,9 +1,7 @@
 package com.example.time_of_war
 
-import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.BitmapFactory
 import android.widget.RemoteViews
@@ -18,14 +16,17 @@ class WidgetProvider : HomeWidgetProvider() {
         appWidgetIds: IntArray,
         widgetData: SharedPreferences
     ) {
-        val viewsPackage = context.packageName
-
         for (appWidgetId in appWidgetIds) {
+
             val views = RemoteViews(
-                viewsPackage,
+                context.packageName,
                 R.layout.widget_layout
             )
 
+            /*
+             * Основне зображення, яке генерується
+             * Flutter через HomeWidget.renderFlutterWidget().
+             */
             val imagePath = widgetData.getString(
                 "widget_image",
                 null
@@ -35,14 +36,18 @@ class WidgetProvider : HomeWidgetProvider() {
             )
 
             if (!imagePath.isNullOrEmpty()) {
+
                 val file = File(imagePath)
 
                 if (file.exists()) {
-                    val bitmap = BitmapFactory.decodeFile(
-                        file.absolutePath
-                    )
+
+                    val bitmap =
+                        BitmapFactory.decodeFile(
+                            file.absolutePath
+                        )
 
                     if (bitmap != null) {
+
                         views.setImageViewBitmap(
                             R.id.widget_image,
                             bitmap
@@ -51,32 +56,9 @@ class WidgetProvider : HomeWidgetProvider() {
                 }
             }
 
-            val intent = Intent(
-                context,
-                MainActivity::class.java
-            ).apply {
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or
-                    Intent.FLAG_ACTIVITY_CLEAR_TOP
-            }
-
-            val pendingIntent = PendingIntent.getActivity(
-                context,
-                appWidgetId,
-                intent,
-                PendingIntent.FLAG_UPDATE_CURRENT or
-                    PendingIntent.FLAG_IMMUTABLE
-            )
-
-            views.setOnClickPendingIntent(
-                R.id.widget_root,
-                pendingIntent
-            )
-
-            views.setOnClickPendingIntent(
-                R.id.widget_image,
-                pendingIntent
-            )
-
+            /*
+             * Оновлюємо сам віджет.
+             */
             appWidgetManager.updateAppWidget(
                 appWidgetId,
                 views
